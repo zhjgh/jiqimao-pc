654c119a68007f943e25e96b13c21b5bdeb244d7
Administrator <1058776714@qq.com>
2017-03-28 13:42:14 +0800

    modify

--- src/js/public.js
+++ src/js/public.js
@@ -1,39 +1,3 @@
-define(['jquery', 'public'], function($, public){
-
-	var exports = {};
-
-	exports.scroll = function(){
-		var scrollTop = $(document).scrollTop();
-		if (scrollTop > 580) {
-			$(".header").css('background', '#fff');
-		} else {
-			$(".header").css('background', '');
-		}
-	};
-
-	exports.addEvent = function(){
-		$(window).resize(function(){
-			var W = $(window).width();
-			if(W < 1280){
-				$(".carousel").find('img').height(480 + 'px');
-			}else{
-				$(".carousel").find('img').height(580 + 'px');
-			}
-		});
-
-		$(window).scroll(function(){
-			exports.scroll();
-		});
-	};
-
-	exports.init = function(){
-		// public.lunbo();
-		exports.scroll();
-		exports.addEvent();
-	};
-
-	return exports;
-});
 define('public', ['jquery'], function($) {
 
 	var public = {};
